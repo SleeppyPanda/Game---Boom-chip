14e5545d3a9cd64b530b737a154fde93513e065e
SleepyPanda <148759175+SleeppyPanda@users.noreply.github.com>
2026-03-30 15:53:01 +0700

    setting and code

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Tên của Scene chọn game (nhớ đặt tên đúng với trong Unity)
+    private string selectSceneName = "SelectScene";
+
+    public void GoToSelectScene()
+    {
+        Debug.Log("Đang chuyển sang màn hình Chọn Game...");
+        SceneManager.LoadScene(selectSceneName);
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Mở bảng Cài đặt (Bật/tắt âm thanh, rung...)");
+        // Gọi UI Panel Settings ra (sẽ code thêm nếu cần)
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Thoát game!");
+        Application.Quit();
+    }
+}
